21de82407b97ccf9f3d1842a2ab6e809cbb6a5a7
1-Bowen <marcus_m0201@hotmail.com>
2019-12-05 12:34:16 -0500

    +tutorial level, range/melee AI

---
+++ Source/MyTestProject/BowenMine.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BowenMine.h"
+
+// Sets default values
+ABowenMine::ABowenMine()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABowenMine::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABowenMine::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
